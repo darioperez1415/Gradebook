35a8ae33ba8cf1c62b0983435cd68ec38d47b338
Dario Perez <darioperez1415@gmail.com>
2022-11-29 16:54:55 -0600

    Saving work

--- test/Gradebook.Test/BookTests.cs
+++ test/Gradebook.Test/BookTests.cs
@@ -4,25 +4,36 @@ using GradeBook;
 
 namespace Gradebook.Test
 {
-public class BookTests
+    public class BookTests
     {
         [Fact]
-            public void BookCalculatesAnAverageGrade()
-        {   
-        // arrange
-        var book = new Book(" ");
-        book.AddGrade(89.1);
-        book.AddGrade(90.5);
-        book.AddGrade(77.3);
+        public void BookCalculatesAnAverageGrade()
+        {
+            // arrange
+            var book = new Book(" ");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
 
-        // act 
-        var result = book.GetStatistics();
+            // act 
+            var result = book.GetStatistics();
 
-        // assert
-        Assert.Equal(85.6, result.Average,1);
-        Assert.Equal(90.5, result.High, 1);
-        Assert.Equal(77.3, result.Low, 1);
-        Assert.Equal('B',result.letter);
+            // assert
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
+            Assert.Equal('B', result.letter);
         }
+
+
+        [Fact]
+        public void SomeTest()
+        {
+            Book myBook = new Book("test");
+            // do stuff
+            // set brakpoints
+            // verify output
+        }
+
     }
 }
